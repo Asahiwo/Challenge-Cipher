825b9d6d32a81c6a9f86d7f89f6eb47c4034f215
Asahiwo <andresmorgatz@gmail.com>
2024-02-21 20:55:04 -0300

    alura challenge

---
+++ app.js
@@ -0,0 +1,38 @@
+function encBtn() {
+    const inputText = document.getElementById("input");
+    const output = encrypt(inputText.value);
+    return document.getElementById("output").value = output;
+}
+
+function decBtn() {
+    const inputText = document.getElementById("input");
+    const output = decrypt(inputText.value);
+    return document.getElementById("output").value = output;
+}
+
+function encrypt(input) {
+    const output = input
+    .replaceAll("e", "enter")
+    .replaceAll("i", "imes")
+    .replaceAll("a", "ai")
+    .replaceAll("o", "ober")
+    .replaceAll("u", "ufat"); 
+    return output;
+}
+
+function decrypt(input) {
+    const output = input
+    .replaceAll("enter", "e")
+    .replaceAll("imes", "i")
+    .replaceAll("ai", "a")
+    .replaceAll("ober", "o")
+    .replaceAll("ufat", "u");
+    return output;
+}
+
+function copyBtn(input) {
+    const copy = document.getElementById("output");
+    copy.select();
+    copy.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copy.value);
+}
